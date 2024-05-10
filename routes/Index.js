afabedaa81fe14d67923991fadd83d345a528138
Nicodemus <pronic@live.com>
2024-05-10 05:45:26 -0500

    index.js comments

--- routes/Index.js
+++ routes/Index.js
@@ -1,9 +1,14 @@
+// Importing the required modules
 const router = require('express').Router(); 
 const apiRoutes = require('./Index');
+
+// Setting up the API routes
 router.use('/api',apiRoutes);
+
+// Handling the 404 Not Found error
 router.use((req, res)=>{
     return res.status(404).send('Not found');
-
 });
 
+// Exporting the router module
 module.exports = router;
